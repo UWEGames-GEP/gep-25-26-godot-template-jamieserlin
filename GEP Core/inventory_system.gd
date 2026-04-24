b2556e282b84400bf0011becfbf3c67564c86b14
jamie <James2.Serlin@live.uwe.ac.uk>
2026-04-24 06:07:38 +0100

    before ui positioning

--- GEP Core/inventory_system.gd
+++ GEP Core/inventory_system.gd
@@ -4,12 +4,16 @@ extends Node
 @onready var player: Node = %Player
 @export var inventory_open: bool = false
 
+func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
+
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("inventory"):
 		inventory_open = !inventory_open
 		
 		if inventory_open:
 			inventory_scene.visible = true
+			get_tree().paused = true #pauses game!
 			
 			# Stop player input
 			player.set_process_input(false)
@@ -21,6 +25,7 @@ func _input(event: InputEvent) -> void:
 		else:
 			inventory_scene.visible = false
 			
+			get_tree().paused = false
 			# Restore player input
 			player.set_process_input(true)
 			player.set_process_unhandled_input(true)
